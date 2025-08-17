fdd79f4c4429d3bdb84e74fa6f476b334a6dbd59
MuhammadTharwat <mt@ncd.io>
2025-08-17 19:30:14 +0100

    Added BootROM app

---
+++ HAL/mcu/32-Bit/Nios-V/alinx_minimal_sys/BootROM-App/app/BootROM.cpp
@@ -0,0 +1,40 @@
+/**
+ ******************************************************************************
+ * @file			app.c
+ * @description	This file implements the blinky demo for NIOS-V CPU
+ * @author			Muhammad Tharwat
+ * @version		V0.0
+ * @date			17-Jun-2025
+ ******************************************************************************
+ */
+
+
+/***********************************************  Defines    **************************************************/
+
+/***********************************************  Constants   *************************************************/
+
+/******************************************* Local Global Variables *******************************************/
+
+/******************************************* Local functions prototypes **************************************/
+
+/******************************************* Local function implementation ***********************************/
+
+#include <alt_types.h>
+#include <altera_avalon_pio_regs.h>
+#include <stdio.h>
+#include "system.h"
+#include "sys/alt_sys_wrappers.h"
+
+int main(void)
+{
+
+  IOWR_ALTERA_AVALON_PIO_DIRECTION(PIO_0_BASE,0xF);
+  IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x0F);
+  while (1)
+  {
+    ALT_USLEEP(100000);
+    IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x05);
+    ALT_USLEEP(100000);
+    IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x0F);
+  }
+}

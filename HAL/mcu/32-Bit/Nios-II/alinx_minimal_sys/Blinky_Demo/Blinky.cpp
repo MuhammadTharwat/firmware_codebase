cd5a7c05ec6dbe10eb2cee88dc04cded5120eeab
MuhammadTharwat <mt@ncd.io>
2025-10-05 22:25:45 +0100

    Added blinky demo to run from DDR

--- HAL/mcu/32-Bit/Nios-II/alinx_minimal_sys/Blinky_Demo/Blinky.cpp
+++ HAL/mcu/32-Bit/Nios-II/alinx_minimal_sys/Blinky_Demo/Blinky.cpp
@@ -1,10 +1,10 @@
 /**
  ******************************************************************************
- * @file			app.c
- * @description	This file implements the Boot ROM application for Alinx NIOS-II BSP
+ * @file			Blinky.c
+ * @description	This file implements the blinky demo for alinx NIOS-II BSP
  * @author			Muhammad Tharwat
  * @version		V0.0
- * @date			17-Jun-2025
+ * @date			05-October-2025
  ******************************************************************************
  */
 
@@ -24,14 +24,10 @@
 #include "bsp_uart.hpp"
 #include "bsp_spi.hpp"
 #include "diagnostic_log.hpp"
-#include "m25p64.hpp"
 #include "general_includes.hpp"
 /***********************************************  Defines    **************************************************/
 
 /***********************************************  Constants   *************************************************/
-static constexpr std::uint32_t gu32_spl_start_addr = UINT32_C(0x400000);
-static constexpr std::size_t gsz_spl_size = 0x100000;
-static constexpr std::size_t gsz_buff_size = 128;
 
 /******************************************* Local Global Variables *******************************************/
 //static bsp::sys *pobj_sys{&bsp::sys::get_instance()};
@@ -43,13 +39,6 @@ static bsp::led gobj_led_3(bsp::get_led_dev(3));
 /*UART */
 static bsp::uart gobj_uart0(bsp::get_uart_dev(0));
 diagnostic_logger gobj_diagnostic_logger(gobj_uart0);
-
-/*SPI*/
-static bsp::spi gobj_spi(bsp::get_spi_dev(0));
-static bsp::output gobj_output(bsp::get_io_dev(0));
-static m25p64 gobj_m25p64(gobj_spi, gobj_output);
-
-static std::uint8_t gau8_buffer[gsz_buff_size];
 /******************************************* Local functions prototypes **************************************/
 
 /******************************************* Local function implementation ***********************************/
@@ -72,17 +61,13 @@ int main(void)
   
   gobj_uart0.init(str_uart_init);
 
-  _ASSERT(GENERIC_SUCCESS == gobj_m25p64.verify_electonic_signature());
-  _ASSERT(GENERIC_SUCCESS == gobj_m25p64.verify_identification());
-  _ASSERT(GENERIC_SUCCESS == gobj_m25p64.read(gu32_spl_start_addr, gau8_buffer, gsz_buff_size));
-
   while (true)
   {
     volatile uint32_t u32_cnt;
-    for(u32_cnt = 0u; u32_cnt < 200000; u32_cnt++);
+    for(u32_cnt = 0u; u32_cnt < 100000; u32_cnt++);
     gobj_led_0.toggle();
     gobj_led_3.toggle();
-    for(u32_cnt = 0u; u32_cnt < 200000; u32_cnt++);
+    for(u32_cnt = 0u; u32_cnt < 100000; u32_cnt++);
     gobj_led_1.toggle();
     gobj_led_2.toggle();
   }

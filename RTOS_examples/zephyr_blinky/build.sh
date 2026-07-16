49b56e29f1d994844f449ae973481500d9ee7a81
MUA-TPS <mabdulfattah@turbopowersystems.com>
2026-07-16 15:48:44 +0100

    completed the support for the custom Nordic unit

--- RTOS_examples/zephyr_blinky/build.sh
+++ RTOS_examples/zephyr_blinky/build.sh
@@ -1,6 +1,6 @@
 source ./venv/bin/activate
 
-export ZEPHYR_BASE=/home/muhammad/firmware_codebase/RTOS/zephyr
+source ../../RTOS/zephyr/zephyr-env.sh
 export GNUARMEMB_TOOLCHAIN_PATH=/home/muhammad/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux/gcc-arm-none-eabi-10.3-2021.10
 # Now run your standard cmake step:
 cmake -GNinja -DBOARD=$1 -BBuild -DZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb

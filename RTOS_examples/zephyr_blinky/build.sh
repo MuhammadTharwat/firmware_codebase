70bb82e8f1a9333c06ee20fa4b71d5bd93d84ab7
MuhammadTharwat <mt@ncd.io>
2026-07-18 15:30:13 +0100

    Fixed DTS and the blinky is running successfully

--- RTOS_examples/zephyr_blinky/build.sh
+++ RTOS_examples/zephyr_blinky/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 source ../../RTOS/sdk-zephyr/zephyr-env.sh
 export GNUARMEMB_TOOLCHAIN_PATH=/opt/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/
 export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb

bdd64db394ff63e31b7f6da12bf4d834f31c1d9e
MuhammadTharwat <mt@ncd.io>
2026-07-18 13:22:44 +0100

    Added corrections

--- RTOS_examples/zephyr_blinky/build.sh
+++ RTOS_examples/zephyr_blinky/build.sh
@@ -1,4 +1,4 @@
-source ../../RTOS/zephyr/zephyr-env.sh
+source ../../RTOS/sdk-zephyr/zephyr-env.sh
 export GNUARMEMB_TOOLCHAIN_PATH=/opt/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/
 export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
 # Now run your standard cmake step:

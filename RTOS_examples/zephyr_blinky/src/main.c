a1d835fc03abdb449f8615dd047e9dc0c88ffd22
MuhammadTharwat <mt@ncd.io>
2026-07-19 11:41:26 +0100

    Removed aliases

--- RTOS_examples/zephyr_blinky/src/main.c
+++ RTOS_examples/zephyr_blinky/src/main.c
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
@@ -11,8 +5,7 @@
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#define LED0_NODE DT_NODELABEL(led_a)
 
 /*
  * A build error on this line means your board is unsupported.
@@ -25,18 +18,22 @@ int main(void)
 	int ret;
 	bool led_state = true;
 
-	if (!gpio_is_ready_dt(&led)) {
+	if (!gpio_is_ready_dt(&led))
+	{
 		return 0;
 	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
+	if (ret < 0)
+	{
 		return 0;
 	}
 
-	while (1) {
+	while (1)
+	{
 		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
+		if (ret < 0)
+		{
 			return 0;
 		}
 

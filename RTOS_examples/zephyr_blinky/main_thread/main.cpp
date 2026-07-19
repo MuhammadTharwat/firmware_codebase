8b2a74f94b4d03c9bc91828c7486522f97b302b9
MuhammadTharwat <mt@ncd.io>
2026-07-19 18:48:47 +0100

    Added ETL and converted the files to C++

--- RTOS_examples/zephyr_blinky/main_thread/main.cpp
+++ RTOS_examples/zephyr_blinky/main_thread/main.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
-#include "ui_thread.h"
+#include "ui_thread.hpp"
 
-
-int main(void)
+extern "C" int main(void)
 {
 	ui_thread_init();
 	return 0;

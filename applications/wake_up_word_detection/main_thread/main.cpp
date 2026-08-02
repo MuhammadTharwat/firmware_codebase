06430d825f9b9bacd5782f4b7e2eda706e024336
MuhammadTharwat <mt@ncd.io>
2026-08-02 16:05:48 +0100

    Added Tensor Flow Lite (Zephyr Fork)

--- applications/wake_up_word_detection/main_thread/main.cpp
+++ applications/wake_up_word_detection/main_thread/main.cpp
@@ -2,6 +2,11 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+#include <tensorflow/lite/micro/micro_log.h>
+#include <tensorflow/lite/micro/micro_interpreter.h>
+#include <tensorflow/lite/micro/micro_mutable_op_resolver.h>
+#include <tensorflow/lite/schema/schema_generated.h>
+
 extern "C" int main(void)
 {
 	return 0;
